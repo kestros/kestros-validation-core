496075ad1848fa1e5b274177a74459b4136c7ae0
Danny Moulton <danny.moulton@kestros.io>
2025-11-25 17:12:14 -0500

    fixed potential NPE

--- src/main/java/io/kestros/commons/validation/core/models/impl/ValidatorResultImpl.java
+++ src/main/java/io/kestros/commons/validation/core/models/impl/ValidatorResultImpl.java
@@ -127,6 +127,9 @@ public class ValidatorResultImpl implements ValidatorResult {
   @Nonnull
   @Override
   public List<ValidatorResult> getBundled() {
+    if( bundled == null) {
+      return Collections.emptyList();
+    }
     return new ArrayList<>(bundled);
   }
 
